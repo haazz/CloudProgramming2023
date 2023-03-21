28665db4891857d96b1a62adab7c23a9a3ea415d
haazz <gkwldnjs121@gmail.com>
2023-03-21 17:45:47 +0900

    수정

--- do_it_django_prj/blog/views.py
+++ do_it_django_prj/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
+
+class PostList(ListView):
+    model = Post
 
 def index(request):
     posts = Post.objects.all().order_by("-pk")
