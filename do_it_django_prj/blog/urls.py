c252d6c5a5eff4002b3e1e17167eca0270df733a
haazz <gkwldnjs121@gmail.com>
2023-03-28 17:09:34 +0900

    add img in list

--- do_it_django_prj/blog/urls.py
+++ do_it_django_prj/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_num>/', views.PostDetail.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     #path('<int:post_num>/', views.single_post_page),
 ]
--- do_it_django_prj/blog/views.py
+++ do_it_django_prj/blog/views.py
@@ -3,31 +3,9 @@ from .models import Post
 
 class PostList(ListView):
     model = Post
-    template_name = 'blog/index.html'
+    ordering = '-pk'
 
 class PostDetail(DetailView):
     model = Post
 
-"""
-def index(request):
-    posts = Post.objects.all().order_by("-pk")
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-"""
-
-def single_post_page(request, post_num):
-    post = Post.objects.get(pk=post_num)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
 
