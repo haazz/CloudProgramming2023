15cf11db24f0be6576266ef46b534055aeeea827
haazz <gkwldnjs121@gmail.com>
2023-04-04 15:06:29 +0900

    update directory

--- do_it_django_prj/blog/urls.py
+++
@@ -1,9 +0,0 @@
-# urlconf for blog
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:pk>/', views.PostDetail.as_view()),
-    path('', views.PostList.as_view()),
-    #path('<int:post_num>/', views.single_post_page),
-]
--- do_it_django_prj/single_pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main),
-    #path('', views.landing),
-]
